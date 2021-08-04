a6e3488827919dc326469199373586cd45eae3e4
mara <ms.marafinkel@gmail.com>
2021-08-04 08:52:02 -0700

    Add greeting container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import {
-  Route,
-  Redirect,
-  Switch,
-  Link,
-  HashRouter
-} from 'react-router-dom';
+import React from "react";
+import GreetingContainer from "./greeting/greeting_container";
+
 const App = () => (
   <div>
-    <header>
-        <h1>HashMaps</h1>
-
-    </header>
+    <h1 align="center">Welcome to HashMaps where we tell <u>you</u> where to buy the devil's jazz lettuce!! !!! !</h1>
+    <h2>Decriminalize it! End the drug war now!</h2>
+    <h3 align="right">I'm high as a mfing kite baybeeeee</h3>
+    <h4>Give job now give give!! give!!!</h4>
+    <h5 align="center"><i>~ ~ ~ Sativa to change the things I can, and indica to accept the things I cannot. ~ ~ ~</i></h5>
+    <GreetingContainer/>
   </div>
 );
 
