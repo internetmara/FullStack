477f761bd0357742a0253a89908e0e7e2bea64dd
mara <ms.marafinkel@gmail.com>
2021-10-26 19:20:21 -0700

    style checkout popup

--- frontend/components/orders/popup_checkout.jsx
+++ frontend/components/orders/popup_checkout.jsx
@@ -5,9 +5,9 @@ class PopupCheckout extends React.Component {
   render() {
     return (
       <div className='popup'>
-        <div className='popup_inner'>
-          <div>
-            <div className='popup-header'>Get some Weed!</div>
+        <div className='popup-inner'>
+          <div className='popup-inner-2'>
+            <div className='popup-header-checkout'>HashMaps is an app clone that unfortunately does not sell actual weed. Maybe one day, but until then feel free to keep browsing!</div>
             <div className='popup-gif'><iframe src="https://giphy.com/embed/d3mm7z65NYqd9gwU" width="250" height="250" frameBorder="0" className="giphy-embed" allowFullScreen></iframe></div>
             <div className='popup-close'><button className='popup-button' onClick={() => { this.props.closePopup; this.props.clearOrder() }}><Link to='/products' className='continue-shopping'>Continue Shopping</Link> </button></div>
           </div>
