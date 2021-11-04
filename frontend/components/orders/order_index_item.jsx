6317ce9c9811904d1745134e7d90581d626e25be
mara <ms.marafinkel@gmail.com>
2021-11-03 19:11:21 -0700

    Display less product info in cart

--- frontend/components/orders/order_index_item.jsx
+++ frontend/components/orders/order_index_item.jsx
@@ -6,13 +6,13 @@ class OrderIndexItem extends React.Component {
     return(
       <div className="order-left">
         <div className="order-products">
-          <h1 className="prod_name">{prod_name}</h1>
-          <h1 className="id">{id}</h1>
           <h1 className="dispo_name">{dispo_name}</h1>
+          <h1 className="prod_name">{prod_name}</h1>
+          {/* <h1 className="id">{id}</h1> */}
           <h1 className="quantity">{quantity}</h1>
           <img className="url" src={url}/>
-          <h1 className="dispo_id">{dispo_id}</h1>
-          <h1 className="prod_id">{prod_id}</h1>
+          {/* <h1 className="dispo_id">{dispo_id}</h1> */}
+          {/* <h1 className="prod_id">{prod_id}</h1> */}
         </div>
       </div>
     )
