1aa878002ca4ee6bc483edea0a26af32a5b1a87c
mara <ms.marafinkel@gmail.com>
2021-11-04 01:41:32 -0700

    Styling

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -8,7 +8,7 @@ export default ({about}) => (
       <a href="https://internetmara.github.io/"><img className="bio-pic" src="images/Mara.jpeg"/></a>
       <br/>
       <p className="bio-text">Hi, my name is Mara and I am a fullstack developer. 
-      {/* <br/> */}
+      <br/>
       <br/>
       I really enjoy working working with frontend frameworks such as React, and backend frameworks such as Rails. My favorite languages to code in are JavaScript and Ruby. 
       <br/>
