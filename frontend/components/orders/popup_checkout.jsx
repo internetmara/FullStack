a1ca8bcfafa18059401d44360004c1d09f83b8eb
mara <ms.marafinkel@gmail.com>
2021-11-03 20:48:40 -0700

    Add popup button close

--- frontend/components/orders/popup_checkout.jsx
+++ frontend/components/orders/popup_checkout.jsx
@@ -7,9 +7,12 @@ class PopupCheckout extends React.Component {
       <div className='popup'>
         <div className='popup-inner'>
           <div className='popup-inner-2'>
+            <button className='popup-close' onClick={() => { this.props.closePopup; this.props.clearOrder() }}>X</button>
             <div className='popup-header-checkout'>HashMaps is an app clone that unfortunately does not sell actual weed. Maybe one day, but until then feel free to keep browsing!</div>
             <div className='popup-gif'><iframe src="https://giphy.com/embed/d3mm7z65NYqd9gwU" width="250" height="250" frameBorder="0" className="giphy-embed" allowFullScreen></iframe></div>
-            <div className='popup-close'><button className='popup-button' onClick={() => { this.props.closePopup; this.props.clearOrder() }}><Link to='/products' className='continue-shopping'>Continue Shopping</Link> </button></div>
+            <div className='continue-shopping'>
+              <button className='popup-button'><Link to='/products'>Continue Shopping</Link></button>
+            </div>
           </div>
         </div>
       </div>
