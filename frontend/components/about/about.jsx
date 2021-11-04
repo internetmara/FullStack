8b320e2465d67de7dcfb786f6380d3844eb593bf
mara <ms.marafinkel@gmail.com>
2021-11-04 00:56:08 -0700

    Style about page

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -4,6 +4,17 @@ import Footer from '../footer/footer';
 export default ({about}) => (
   <div className="about">
     <div className="about-text">Welcome to HashMaps, a clone of&nbsp;<a href="https://weedmaps.com/">Weedmaps</a>!</div>
+    <div className="bio">
+      <img className="bio-pic" src="images/Mara.jpeg"/>
+      <br/>
+      <p className="bio-text">Hi, my name is Mara and I am a fullstack developer. 
+      <br/>
+      <br/>
+      I really enjoy working working with frontend frameworks such as React, and backend frameworks such as Rails. My favorite languages to code in are JavaScript and Ruby. 
+      <br/>
+      <br/>
+      I live in California's Bay Area with my dog Jellybean and have spent the last half-decade educating people and patients on the many complex and disparate issues within the cannabis industry.</p>
+    </div>
     <Footer/>
   </div>
 )
