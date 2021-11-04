65254e69e1444560c7fdaf40e7adcf34c833c989
mara <ms.marafinkel@gmail.com>
2021-11-03 19:17:29 -0700

    Add flex display

--- frontend/components/orders/order_index_item.jsx
+++ frontend/components/orders/order_index_item.jsx
@@ -5,11 +5,12 @@ class OrderIndexItem extends React.Component {
     const { id, prod_name, dispo_name, quantity, url, dispo_id, prod_id, userOrders } = this.props
     return(
       <div className="order-left">
+          <h1 className="dispo_name">Ordering from: {dispo_name}</h1>
+          <br/>
         <div className="order-products">
-          <h1 className="dispo_name">{dispo_name}</h1>
           <h1 className="prod_name">{prod_name}</h1>
           {/* <h1 className="id">{id}</h1> */}
-          <h1 className="quantity">{quantity}</h1>
+          <h1 className="quantity">Quantity: {quantity}</h1>
           <img className="url" src={url}/>
           {/* <h1 className="dispo_id">{dispo_id}</h1> */}
           {/* <h1 className="prod_id">{prod_id}</h1> */}
