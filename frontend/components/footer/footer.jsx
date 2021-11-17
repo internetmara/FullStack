302faf45187ce80ae2cf057f2ae8848c537b6f7c
mara <ms.marafinkel@gmail.com>
2021-11-17 11:10:59 -0800

    Fix typos

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ footer }) => {
   return (
     <div className="product-footer">
-      <h6 className="footer-text">Copyright © 2021 HashMaps. HashMaps is a registered trademarks of Jellybean Management Group, LLC. All Rights Reserved.</h6>
+      <h6 className="footer-text">Copyright © 2021 HashMaps. HashMaps is a registered trademark of Jellybean Management Group, LLC. All Rights Reserved.</h6>
       <div className="social-container">
         <a href="https://internetmara.github.io/" target="_blank"><img className="socials" src="images/icons/Jellycopy.png"/></a>
         <a href="https://www.linkedin.com/in/marafinkel/" target="_blank"><img className="socials" src="images/icons/Linkedin.png"/></a>
