48e9945315d873f0f5983c86775de16663e49679
mara <ms.marafinkel@gmail.com>
2021-11-17 11:09:39 -0800

    Fix typos

--- frontend/components/quantity_selector/quantity_selector.jsx
+++ frontend/components/quantity_selector/quantity_selector.jsx
@@ -20,7 +20,7 @@ class QuantitySelector extends React.Component {
 
     return (
         <div className="quantity-selector-wrapper">
-          <button disabled="" data-test-id="segment-minus-button" data-testid="minus-button" aria-label="Reduce Quanity by 1" className="quantity-selector-button" onClick={this.decrement}>
+          <button disabled="" data-test-id="segment-minus-button" data-testid="minus-button" aria-label="Reduce Quantity by 1" className="quantity-selector-button" onClick={this.decrement}>
            <div className="sc-bdnxRM jvCTkj" style={{width:20, height:20}}>
              <svg width="100%" height="100%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                <polygon fill="#666666" points="448,224 64,224 64,288 448,288"/>
